13936946158e2b1afc643a3fc6f0ba0bd1dba5a8
Shille88 <shille_88@hotmail.com>
2016-12-15 15:16:20 +0100

    forecastapp
    average increase

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 
 public class Main {
 	
+	static int[] year = new int[6];
+	static int years;
+	
+
 	public Main() {
-		
 	}
 	
 	public static void main(String[] args) {
-	}
+		
+		year[0] = 618385;
+		year[1] = 491956;
+		year[2] = 371631;
+		year[3] = 389200;
+		year[4] = 359598;
+		year[5] = 313410;
+		
+		years = year.length;
 
+		double j = 0;
+		
+		for(int i=0; i<years-1; i++){
+			double current = (double) (year[i] - year[i+1])/year[i+1];
+			System.out.println(current);
+			j = j + (double) (year[i] - year[i+1])/year[i+1];
+		}
+		j=j/years;
+		System.out.println(j);
+	}
 }
